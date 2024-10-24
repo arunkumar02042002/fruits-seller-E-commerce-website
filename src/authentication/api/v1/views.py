8c28f988f6c55665cba5eb23cd55c7da62f2798c
arunkumar02042002 <arun.kumar.2403gg@gmail.com>
2024-10-24 10:22:07 +0530

    Added Password and Email Validator APIs.

--- src/authentication/api/v1/views.py
+++ src/authentication/api/v1/views.py
@@ -4,27 +4,26 @@ from rest_framework import status
 
 from .serializers import EmailValidatorSerializer, PasswordValidatorSerializer
 
+
 class EmailValidatorApiView(GenericAPIView):
     serializer_class = EmailValidatorSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response({
-            "status":"success",
-            "message":"Email is valid.",
-            "data":{}
-        }, status=status.HTTP_200_OK)
-    
+        return Response(
+            {"status": "success", "message": "Email is valid.", "data": {}},
+            status=status.HTTP_200_OK,
+        )
+
+
 class PasswordValidatorApiView(GenericAPIView):
     serializer_class = PasswordValidatorSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return Response({
-            "status":"success",
-            "message":"Password is valid.",
-            "payload":{}
-        }, status=status.HTTP_200_OK)
-
+        return Response(
+            {"status": "success", "message": "Password is valid.", "payload": {}},
+            status=status.HTTP_200_OK,
+        )
