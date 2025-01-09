8955bb8998077973d8543ffaa145953e275ff49b
Arun Kumar <84587079+arunkumar02042002@users.noreply.github.com>
2025-01-09 19:05:11 +0530

    Added testimonial add view (#3)

--- src/main/views.py
+++ src/main/views.py
@@ -1,10 +1,17 @@
 from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from django.contrib import messages
+
+from . import forms
+
+User = get_user_model()
 
 
+# Create your views here.
 class HomeView(TemplateView):
     """Home view renders index.html"""
     template_name = "main/index.html"
-
 
 class AboutUsView(TemplateView):
     """About view renders about.html"""
