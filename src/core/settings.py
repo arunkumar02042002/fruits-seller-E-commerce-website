03d40993d41d4eb689842ae871c2d370132d87eb
arunkumar02042002 <arun.kumar.2403gg@gmail.com>
2025-01-09 15:07:27 +0530

    Added middleware

--- src/core/settings.py
+++ src/core/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "common.middleware.LogMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
