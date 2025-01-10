ad4dd18ef5b4efe950f0b19c0e759d414d844e7b
Arun Kumar <84587079+arunkumar02042002@users.noreply.github.com>
2025-01-10 13:09:36 +0530

    Added 400 and 500 html pages (#4)

--- src/main/views.py
+++ src/main/views.py
@@ -5,6 +5,7 @@ class HomeView(TemplateView):
     """Home view renders index.html"""
     template_name = "main/index.html"
 
+
 class AboutUsView(TemplateView):
     """About view renders about.html"""
     template_name = "main/about.html"
