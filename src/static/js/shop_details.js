ae32ee4c514a448769e102d4f1e3585aafd9761f
Arun Kumar <arun.kumar.2403gg@gmail.com>
2025-02-11 15:26:17 +0530

    Add Review DOne

--- src/static/js/shop_details.js
+++ src/static/js/shop_details.js
@@ -1,6 +1,9 @@
 const ADD_TO_CART_URL = `${window.location.origin}/api/v1/users/cart/add/`;
 const ADD_TO_CART_BUTTON = document.getElementById('add-to-cart');
 const QUANTITY_INPUT = document.getElementById('quantity-input');
+const ADD_REVIEW_FORM = document.getElementById('add-review-form');
+const ADD_REVIEW_BUTTON = document.getElementById('add-review-button');
+const ReviewTab = document.getElementById('review-container');
 
 function getCookie(name) {
     const value = `; ${document.cookie}`;
@@ -68,3 +71,91 @@ ADD_TO_CART_BUTTON.addEventListener('click', async (e) => {
         alertMessage(error.message, 'alert-danger');
     }
 });
+
+function addReviewToTab(review){
+
+    let reviewCard = document.createElement('div');
+    reviewCard.classList.add('d-flex');
+    let img = document.createElement('img');
+    img.classList.add('img-fluid', 'rounded-circle', 'p-3');
+    img.style.width = '100px';
+    img.style.height = '100px';
+    img.alt = '';
+    img.src = review.profile.profile_picture ? review.profile.profile_picture : '/static/users/profile_pictures/default_profile.png';
+
+    let reviewDiv = document.createElement('div');
+    let date = document.createElement('p');
+    date.classList.add('mb-2');
+    date.style.fontSize = '14px';
+    date.textContent = review.created_at.date;
+
+    let reviewContent = document.createElement('div');
+    reviewContent.classList.add('d-flex', 'justify-content-between');
+    let name = document.createElement('h5');
+    name.textContent = review.profile.user.first_name + review.profile.user.last_name
+    let stars = document.createElement('div');
+    stars.classList.add('d-flex', 'mb-3');
+    for (let i = 0; i < 5; i++){
+        let star = document.createElement('i');
+        star.classList.add('fa', 'fa-star');
+        if (i < review.rating){
+            star.classList.add('text-secondary');
+        }
+        stars.appendChild(star);
+    }
+
+    let reviewText = document.createElement('p');
+    reviewText.textContent = review.review;
+
+    let created_at = document.createElement('p');
+    created_at.classList.add('mb-2');
+    created_at.style.fontSize = '14px';
+    created_at.textContent = review.created_at.date;
+
+
+    reviewContent.appendChild(name);
+    reviewContent.appendChild(stars);
+    reviewDiv.appendChild(created_at);
+    reviewDiv.appendChild(reviewContent);
+    reviewDiv.appendChild(reviewText);
+    reviewCard.appendChild(img);
+    reviewCard.appendChild(reviewDiv);
+    ReviewTab.appendChild(reviewCard);
+
+    return reviewCard;
+}
+
+if (ADD_REVIEW_FORM){
+    ADD_REVIEW_FORM.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        ADD_REVIEW_BUTTON.setAttribute('disabled', 'disabled');
+        ADD_REVIEW_BUTTON.innerHTML = 'Adding Review...';
+        let formData = new FormData(ADD_REVIEW_FORM);
+        ADD_REVIEW_FORM.reset();
+        
+        setTimeout(async () => {
+            let response = await fetch(ADD_REVIEW_FORM.action, {
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-CSRFToken': getCookie('csrftoken')
+                },
+                method: ADD_REVIEW_FORM.method,
+                body: formData,
+            });
+
+           let data = await response.json();
+
+           let reviewCard = addReviewToTab(data.payload);
+    
+            if (response.ok) {
+                alertMessage(data.message, 'alert-success');
+            } else {
+                reviewCard.remove();
+                alertMessage(data.message, 'alert-danger');
+            }
+        
+            ADD_REVIEW_BUTTON.removeAttribute('disabled');
+            ADD_REVIEW_BUTTON.innerHTML = 'Add Review';
+        }, 1000);
+    });
+}
